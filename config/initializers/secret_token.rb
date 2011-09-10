4ec63a9c14d9dc7a1f51009140ab27b9506460cf
davishacker <davishacker86@gmail.com>
2011-09-10 14:33:53 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GardenPower::Application.config.secret_token = 'd88abc313b5fd01c437b34693657d10ecb0e0ad0598d2ee3ebb3d275e03bb9bfa2cc0a0e940123bcd7f0ed43272c3bd04220efa37693de52dd25f94b964a0f5f'
