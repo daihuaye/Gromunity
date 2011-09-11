3757c33193f7461e92d7cf686474aebd3b444458
Alex <alex.nisnevich@gmail.com>
2011-09-10 20:05:28 -0700

    Fixing up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  validates_presence_of :login, :email
+  validates_presence_of :login
   validates_uniqueness_of :email
-  validates_length_of :password, :minimum => 4
 end
 
