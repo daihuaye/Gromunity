b21defa8013ceb411e3c29d00b67ebbb4d2eeebb
Alex <alex.nisnevich@gmail.com>
2011-09-10 19:34:45 -0700

    minor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
-
-  #before_filter :require_user
-
   def index
-  end
+    @posts = Post.all
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @posts }
+    end
+  end
 end
