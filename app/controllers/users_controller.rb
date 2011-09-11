51a8bff114606a9b41e4cf9dcb778e434cd8154e
Alex <alex.nisnevich@gmail.com>
2011-09-11 10:00:26 -0700

    RETURN OF THE PREGPLANT WHAT WHAT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+  
   # GET /users/new
   # GET /users/new.xml
   def new
@@ -15,6 +24,7 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
 
     respond_to do |format|
       format.html # show.html.erb
