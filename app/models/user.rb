1d24c65db10b938f2c991e45e632df10e6b308c3
davishacker <davishacker86@gmail.com>
2011-09-10 20:04:36 -0700

    validation problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  attr_accessible(:login, :email, :password)
+  validates_presence_of :login, :email
+  validates_uniqueness_of :email
+  validates_length_of :password, :minimum => 4
 end
 
